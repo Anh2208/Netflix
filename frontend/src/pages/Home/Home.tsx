0be425ce82f4caa49687a461f4bf989ce0baf100
Tran Kha Phu B1900354 <phuproz348@gmail.com>
2024-04-19 17:06:33 +0700

    phu commit frontend

---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+import Header from "../../components/Header/Header.tsx";
+
+const Home = () => {
+  return (
+    <div className="home_container">
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
