105248821e99d4c81c9e42c4007e64eaa0f9babf
Tran Kha Phu B1900354 <phuproz348@gmail.com>
2024-04-23 12:57:42 +0700

    phu update frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Home from "./pages/Home/Home.tsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 function App() {
   return (
     <>
       <Home />
-      
     </>
   );
 }
---
+++ client/src/pages/Home/Home.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Home.css";
+import Header from "../../components/Home/Home.tsx";
+
+const Home = () => {
+  return (
+    <div className="home_container">
+      <div className="netflix-sans-font-loaded">
+        <div data-uia="loc" lang="vi" dir="ltr">
+          <Header />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
