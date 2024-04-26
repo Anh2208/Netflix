4d9889e864dbc59ee5e6ecdc6187c4a857d086f8
anhvh1412 <anhvh1412@gmail.com>
2024-04-26 08:15:25 +0700

    update css global

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import Home from "./pages/Home/Home.tsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import Profile from "./pages/Profile/Profile.tsx";
 
 function App() {
   return (
     <>
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/user" element={<Profile />} />
+        </Routes>
+      </Router>
     </>
   );
 }
